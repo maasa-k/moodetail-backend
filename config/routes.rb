4e05b563958324eaa130c10471febbbd899b1405
Maasa <maasa.dev@gmail.com>
2020-12-18 22:41:52 -0800

    Set up backend for user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+
   namespace :api do 
     namespace :v1 do 
-      resources :moods do
-        resources :prompts do
-          resources :notes do
+      resources :users, only: [:create, :show, :index] do
+        resources :moods do
+          resources :prompts do
+            resources :notes do
+            end
           end
         end
       end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_010857) do
+ActiveRecord::Schema.define(version: 2020_12_19_061609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 2020_10_05_010857) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "notes"
     t.index ["mood_id"], name: "index_prompts_on_mood_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "notes", "prompts"
